775a45b94edaa9a5ec7cbe679bfc4c0a996e259c
John Jimenez <home@Vishals-MacBook-Pro.local>
2012-08-11 21:15:41 -0700

    include gem spec and renamed files

---
+++ git_manager.gemspec
@@ -0,0 +1,13 @@
+Gem::Specification.new do |s|
+  s.name        = 'git_manager'
+  s.version     = '0.0.0'
+  s.date        = '2012-08-11'
+  s.summary     = "manage git"
+  s.description = "all local branches, but one you are on"
+  s.authors     = ["kikuchiyo"]
+  s.email       = 'kikuchiyo6@gmail.com'
+  s.files       = ["lib/git_manager.rb"]
+  s.test_files  = ["spec/git_manager_spec.rb"]
+  s.homepage    =
+    'http://rubygems.org/gems/git_manager'
+end
--- spec/git_manager_spec.rb
+++ spec/git_manager_spec.rb
@@ -1,34 +1,6 @@
 require 'rubygems'
 require 'rspec'
-
-module Git
-  class Manager
-    attr_accessor :branches, :current_branch, :branches_to_delete
-    def initialize  
-      @branches = `git branch`.split '\n'
-      partition_branches
-    end
-    def partition_branches
-      @current_branch = @branches.select {|branch| branch.match /^\*/}.first
-      @current_branch.chomp!
-      @current_branch.gsub! /\s|\*/, ''
-
-      @branches_to_delete = @branches.reject {|branch| branch.match /^\*/}
-    end
-
-    def delete_branches
-      return false if @branches_to_delete.length == 0
-      @branches_to_delete.each do |branch|
-        delete_branch
-      end
-    end
-
-    def delete_branch branch
-      `git branch -D #{branch}`
-    end
-
-  end
-end
+require 'git_manager'
 
 describe Git::Manager do
   before :each do
@@ -58,3 +30,5 @@ describe Git::Manager do
     @git_manager.delete_branches.should_not == false
   end
 end 
+
+

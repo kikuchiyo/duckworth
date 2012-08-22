da5a552d155fcfa5977592364e8ea96aafff22fd
John Jimenez <johnjimenez@Johns-MacBook-Pro.local>
2012-08-21 18:39:18 -0700

    Blames Pending Specs through standard output for now

--- lib/git_manager.rb
+++ lib/git_manager.rb
@@ -1,29 +1,134 @@
 require 'rubygems'
-module Git
-  class Manager
-    attr_accessor :branches, :current_branch, :branches_to_delete
-    def initialize  
-      @branches = `git branch`.split /\n/
-      partition_branches
-    end
-    def partition_branches
-      @current_branch = @branches.select {|branch| branch.match /^\*/}.first
-      @branches_to_delete = @branches.select {|branch| branch != @current_branch }
-      @current_branch.chomp!
-      @current_branch.gsub! /\s|\*/, ''
-    end
 
-    def delete_branches
-      return false if @branches_to_delete.length == 0
-      @branches_to_delete.each do |branch|
-        delete_branch( branch.gsub /\s/, '' )
+# usage Git::Blames::Pending.new.blame
+# usage Git::Managed::Branch.new.delete_branches
+
+module Git
+  module Managed
+    class Branch
+    end
+  end
+  module Blames
+    def blame
+      self.tasks.each_pair do |key, attributes|
+        puts "\nPending Spec Information:"
+        puts "  Description: #{key}"
+        puts "  Contributors: #{attributes[:contributors].join(', ')}"
+        puts "  File: #{attributes[:spec_file]}"
+        puts "  Line: #{attributes[:line_number]}\n"
       end
     end
-
-    def delete_branch branch
-      `git branch -D #{branch}`
+    class Pending
     end
+    class Blame
+    end
+  end
+end
 
+class Git::Managed::Branch
+  attr_accessor :branches, :current_branch, :branches_to_delete
+  def initialize  
+    @branches = `git branch`.split /\n/
+    partition_branches
+  end
+  def partition_branches
+    @current_branch = @branches.select {|branch| branch.match /^\*/}.first
+    @branches_to_delete = @branches.select {|branch| branch != @current_branch }
+    @current_branch.chomp!
+    @current_branch.gsub! /\s|\*/, ''
+  end
+
+  def delete_branches
+    return false if @branches_to_delete.length == 0
+    @branches_to_delete.each do |branch|
+      delete_branch( branch.gsub /\s/, '' )
+    end
+  end
+
+  def delete_branch branch
+    `git branch -D #{branch}`
+  end
+
+end
+
+class Git::Blames::Pending 
+  attr_accessor :tasks
+  include Git::Blames
+  def initialize
+    @root = "#{File.dirname(__FILE__)}/logs"
+    find_pending_specs_by_logfile_name( "#{@root}/#{find_last_logfile_name}" )
+    find_contributors
+  end
+
+  def find_contributors
+    # @tasks = Git::Blames::Pending.new.tasks
+    contributors = []
+    @tasks.each_pair do |key, attributes|
+      `git blame "#{attributes[:spec_file]}"`.each do |blame|
+        contributor = blame.split(")")[0].split("(")[1]
+        contributor = contributor.split(/\s+\d/)[0]
+        contributors.push( contributor )
+      end
+      @tasks[key][:contributors] = contributors.uniq
+      @tasks[key][:contributors].reject!{ |contributor| contributor == 'Not Committed Yet'}
+    end
+    @tasks
+  end
+
+  def found_pending_marker line
+    line.match( /^[\s\t]*Pending:/ )
+  end
+
+  def found_new_spec_marker line, status
+    ( status == 'start' || status == 'updating spec data' ) && !line.match( /^[\s\t]*#/ )
+  end
+
+  def found_spec_details_marker line, status
+    ( status == 'found new pending spec' || status == 'updating spec data' ) && line.match( /^[\s\t]*\#/ )
+  end
+
+  def find_last_logfile_name
+    begin
+      files = Dir.new @root
+    rescue => e 
+      begin
+      Dir.mkdir @root
+      files = Dir.new @root
+      rescue
+        puts e
+      end
+    end
+    files.to_a.select { |log| log.match /\d{4}-\d{2}-\d{2}_\d{2}-\d{2}-\d{2}/ }.sort.last
+  end
+
+  def find_pending_specs_by_logfile_name logfile_name
+    logfile = File.new( logfile_name )
+    status = ''
+    @tasks = {}
+    task = {}
+
+    logfile.readlines.each do |line|
+
+      if found_pending_marker line
+        status = 'start'
+        
+      elsif found_new_spec_marker line, status
+        status = 'found new pending spec'  
+        task = {}
+        task[:name] = line.chomp
+        task[:details] = []
+
+      elsif found_spec_details_marker line, status
+        status = 'updating spec data'
+        if line.match /(spec.*)[:](\d+)/
+          task[:spec_file] = $1
+          task[:line_number] = $2
+        else
+          task[:details].push line.chomp
+        end
+        @tasks[task[:name]] = task
+      end
+    end
   end
 end
 
--- spec/git_manager_spec.rb
+++ spec/git_manager_spec.rb
@@ -2,33 +2,78 @@ require 'rubygems'
 require 'rspec'
 require 'git_manager'
 
-describe Git::Manager do
-  before :each do
-    @git_manager = Git::Manager.new
-  end
-  it "has branches" do
-    @git_manager.branches.should_not be_nil
-  end    
-  it "has branches array" do
-    @git_manager.branches.class.should == Array
-  end    
-  it "knows about current branch" do
-    @git_manager.current_branch.should == 'master'
+describe Git do
+  describe Git::Managed::Branch do
+    before :each do
+      @git_manager = Git::Managed::Branch.new
+    end
+    it "has branches" do
+      @git_manager.branches.should_not be_nil
+    end    
+    it "has branches array" do
+      @git_manager.branches.class.should == Array
+    end    
+    it "knows about current branch" do
+      @git_manager.current_branch.should == 'master'
+    end 
+    it "knows about branches to delete" do
+      @git_manager.branches_to_delete.should_not be_nil
+    end 
+    it "aborts if branches to delete is empty" do
+      @git_manager.branches_to_delete = []
+      @git_manager.delete_branches.should == false
+    end
+    it "deltes branches to delete if NOT empty" do
+      @fake_branch_name = 'some_fake_branch'
+      Git::Managed::Branch.any_instance.stub(:delete_branch).and_return( true )
+  
+      @git_manager.branches_to_delete = ['some_fake_branch']
+      @git_manager.delete_branches.should_not == false
+    end
   end 
-  it "knows about branches to delete" do
-    @git_manager.branches_to_delete.should_not be_nil
+  
+  describe Git::Blames::Pending do
+    before :each do
+      @test_01 = '  Fake test 1 spec/git_manager_spec.rb'
+      @test_02 = '  Fake test 2 spec/git_manager_spec.rb'
+      @git_blames_pending = Git::Blames::Pending.new
+      @expected_tasks = {
+        @test_01  => {
+          :spec_file  => 'spec/git_manager_spec.rb',
+          :details=>["    # Test 123"],
+          :line_number => '13',
+          :contributors => ['John Jimenez'],
+          :name => "  Fake test 1 spec/git_manager_spec.rb"
+        },
+        @test_02 => {
+          :spec_file  => 'spec/git_manager_spec.rb',
+          :details=>["    # Test 123"],
+          :line_number => '11',
+          :contributors => ['John Jimenez'],
+          :name => "  Fake test 2 spec/git_manager_spec.rb"
+        }
+      }
+    end
+     
+    it "collects pending specs as expected" do
+      @git_blames_pending.tasks[@test_01][:spec_file].should == @expected_tasks[@test_01][:spec_file]
+      @git_blames_pending.tasks[@test_02][:spec_file].should == @expected_tasks[@test_02][:spec_file]
+    end 
+    it "collects pending spec line number as expected" do
+      @git_blames_pending.tasks[@test_01][:line_number].should == @expected_tasks[@test_01][:line_number]
+      @git_blames_pending.tasks[@test_02][:line_number].should == @expected_tasks[@test_02][:line_number]
+    end 
+    it "collects pending spec name as expected" do
+      @git_blames_pending.tasks[@test_01][:spec_name].should == @expected_tasks[@test_01][:spec_name]
+      @git_blames_pending.tasks[@test_02][:spec_name].should == @expected_tasks[@test_02][:spec_name]
+    end 
+    it "collects pending spec contributors as expected" do
+      @git_blames_pending.tasks[@test_01][:contributors].should == @expected_tasks[@test_01][:contributors]
+      @git_blames_pending.tasks[@test_02][:contributors].should == @expected_tasks[@test_02][:contributors]
+    end 
+    it "collects pending spec details as expected" do
+      @git_blames_pending.tasks[@test_01][:details].should == @expected_tasks[@test_01][:details]
+      @git_blames_pending.tasks[@test_02][:details].should == @expected_tasks[@test_02][:details]
+    end 
   end 
-  it "aborts if branches to delete is empty" do
-    @git_manager.branches_to_delete = []
-    @git_manager.delete_branches.should == false
-  end
-  it "deltes branches to delete if NOT empty" do
-    @fake_branch_name = 'some_fake_branch'
-    Git::Manager.any_instance.stub(:delete_branch).and_return( true )
-
-    @git_manager.branches_to_delete = ['some_fake_branch']
-    @git_manager.delete_branches.should_not == false
-  end
 end 
-
-
